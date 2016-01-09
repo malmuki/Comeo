1bfe72afcb6e17369f758856645d3068c1fac67a
Samuel <samuel.theriault.hall@gmail.com>
2016-01-09 10:03:39 -0500

    Added framework for A*

---
+++ csharp-client/csharp-client/Bot/Pathfinder.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CoveoBlitz;
+
+namespace Coveo.Bot
+{
+    public class Pathfinder
+    {
+
+        public GameState gameInfo;
+        private PathfinderTile[][] globalMap;
+        private List<PathfinderTile> openList = new List<PathfinderTile>();
+        private List<PathfinderTile> closedList = new List<PathfinderTile>();
+
+        public Pathfinder(GameState _gameInfo)
+        {
+            gameInfo = _gameInfo;
+            globalMap = new PathfinderTile[gameInfo.board.GetLength(0)][gameInfo.board.GetLength(1)];
+
+            //Initialise pathfinding board
+            for (int i = 0; i < gameInfo.board.GetLength(0); ++i)
+            {
+                for (int j = 0; j < gameInfo.board.GetLength(1); ++j)
+                {
+                    globalMap[i][j] = new PathfinderTile(i, j, gameInfo.board[i][j]);
+                }
+            }
+        }
+
+        public string GetNextMoveToGetToDestination(int _destinationX, int _destinationY)
+        {
+            AddSurroundingTilesToOpenList(gameInfo.myHero.pos.x, gameInfo.myHero.pos.y);
+
+            PathfinderTile lastTileAdded;
+
+            do
+            {
+                AddLowestHeuristicToClosedListFromOpenList();
+
+                lastTileAdded = closedList.ElementAt(closedList.Count - 1);
+
+
+            } while (openList.Count > 0 && (lastTileAdded.x != _destinationX || lastTileAdded.y != _destinationY));
+
+            return Direction.Stay;
+        }
+
+        //private List<PathfinderTile> GetCompletePath()
+        //{
+            
+        //}
+
+        private void AddSurroundingTilesToOpenList(int _x, int _y)
+        {
+            AddSpecificTileToOpenListIfValid(_x - 1, _y);
+            AddSpecificTileToOpenListIfValid(_x + 1, _y);
+            AddSpecificTileToOpenListIfValid(_x, _y - 1);
+            AddSpecificTileToOpenListIfValid(_x, _y + 1);
+        }
+
+        private void AddSpecificTileToOpenListIfValid(int _x, int _y)
+        {
+            if (_x >= 0 && _y >= 0)
+            {
+                if (_x < gameInfo.board.GetLength(0) && _y < gameInfo.board.GetLength(1))
+                {
+                    if (globalMap[_x][_y].IsTraversable(gameInfo.myHero.life, gameInfo.myHero.id))
+                    {
+                        openList.Add(globalMap[_x][_y]);
+                    }
+                }
+            }
+        }
+
+        private void AddLowestHeuristicToClosedListFromOpenList()
+        {
+            PathfinderTile bestTile = openList.Where(_x => _x == openList.Min()).ElementAt(0);
+
+            if ()
+            {
+
+            }
+            bestTile.parent = closedList.ElementAt(closedList.Count - 1);
+
+            closedList.Add();
+        }
+
+    }
+}
---
+++ csharp-client/csharp-client/Bot/PathfinderTile.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using CoveoBlitz;
+
+namespace Coveo.Bot
+{
+    class PathfinderTile
+    {
+        public int x;
+        public int y;
+        public Tile type;
+        public PathfinderTile parent = null;
+
+        public PathfinderTile(int _x, int _y, Tile _type)
+        {
+            x = _x;
+            y = _y;
+            type = _type;
+        }
+
+        public int GetCostToTraverse(int _heroHP, int _heroID)
+        {
+            if (!IsTraversable(_heroHP, _heroID))
+            {
+                throw new Exception();
+            }
+
+            if (type == Tile.FREE)
+                return 1;
+
+            if (type == Tile.SPIKES)
+                return 10;
+
+            //Tavern
+            return 5;
+        }
+
+        public bool IsTraversable(int _heroHP, int _heroID)
+        {
+            //Spikes are not traversable if you are at 10 or less HP
+            if (type == Tile.FREE || type == Tile.TAVERN || type == Tile.GOLD_MINE_NEUTRAL)
+                return true;
+
+            if (type == Tile.GOLD_MINE_1 && _heroID != 1)
+                return true;
+
+            if (type == Tile.GOLD_MINE_2 && _heroID != 2)
+                return true;
+
+            if (type == Tile.GOLD_MINE_3 && _heroID != 3)
+                return true;
+
+            if (type == Tile.GOLD_MINE_4 && _heroID != 4)
+                return true;
+
+            if (type == Tile.SPIKES && _heroHP >= 10)
+                return true;
+
+            return false;
+
+            //type == Tile.IMPASSABLE_WOOD  || type == Tile.HERO_1 || type == Tile.HERO_2 || type == Tile.HERO_3 || type == Tile.HERO_4
+        }
+
+    }
+}
